96353e2c7a31fb584131493c6d4c4561c017eef1
Rodrigo Andrade <rodrigoandrade@fb.com>
2019-07-08 15:11:16 -0700

    Initial Commit

---
+++ offbrand-instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  offbrand-instagram
+//
+//  Created by rodrigoandrade on 7/8/19.
+//  Copyright © 2019 rodrigoandrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ offbrand-instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  offbrand-instagram
+//
+//  Created by rodrigoandrade on 7/8/19.
+//  Copyright © 2019 rodrigoandrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
